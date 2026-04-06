0bf7918a10507f89fcc72ef216e8c8a026ec93ce
MACALLAN-FE-Cris <cris@smartbit.one>
2026-04-06 17:45:23 +0700

    refactor: change arrow function to default function for Test Robot pages

--- app/test-robot-index/page.tsx
+++ app/test-robot-index/page.tsx
@@ -1,8 +1,8 @@
-export const TestRobotIndexPage = () => {
+export default function TestRobotIndexPage() {
   return (
     <div>
       <h1>Test Robot Indexed</h1>
       <p>This is the test robot page indexed.</p>
     </div>
   );
-};
+}
--- app/test-robot/page.tsx
+++ app/test-robot/page.tsx
@@ -1,8 +1,8 @@
-export const TestRobotPage = () => {
+export default function TestRobotPage() {
   return (
     <div>
       <h1>Test Robot</h1>
       <p>This is the test robot page.</p>
     </div>
   );
-};
+}
