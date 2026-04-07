216ecbe29db64b27ce7b34079e1dc284aa20f3e2
MACALLAN-FE-Cris <cris@smartbit.one>
2026-04-07 11:35:36 +0700

    refactor: remove metadata for Test Robot page to allow indexing

--- app/test-robot-index/page.tsx
+++ app/test-robot-index/page.tsx
@@ -1,3 +1,11 @@
+export const metadata = {
+  title: "Test Robot",
+  description: "This is the test robot page.",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 export default function TestRobotIndexPage() {
   return (
     <div>
